04c731efe73bc98fe9cb441aa507a2f3b10beea0
n4cl <devn4cl@gmail.com>
2023-07-11 00:49:02 +0900

    Add: 形態素解析ツールを追加

---
+++ ch04/torknaizer.py
@@ -0,0 +1,26 @@
+import MeCab
+tagger = MeCab.Tagger("-d /usr/lib/aarch64-linux-gnu/mecab/dic/mecab-ipadic-neologd") # TODO: 環境に合わせて変更する
+
+def torkenizer(text):
+    """
+    Mecab で形態素解析を実施する
+    """
+    nodes = tagger.parseToNode(text)
+    result = []
+    while nodes:
+        if nodes.surface != "":
+            morpheme = {}
+            s = nodes.feature.split(",")
+            morpheme["surface"] = nodes.surface
+            morpheme["base"] = s[6]
+            morpheme["pos"] = s[0]
+            morpheme["pos1"] = s[1]
+            result.append(morpheme)
+        nodes = nodes.next
+    return result
+
+
+if __name__ == '__main__':
+    res = torkenizer("これはテストです。")
+    for m in res:
+        print(m["surface"], m["base"], m["pos"], m["pos1"])
